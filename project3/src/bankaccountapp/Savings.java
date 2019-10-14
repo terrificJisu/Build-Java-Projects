2aca80d1229761caec813687d58b7793726bdf83
JiSu Choi <cjs7250@gmail.com>
2019-10-14 15:55:44 +1000

    project 3

---
+++ project3/src/bankaccountapp/Savings.java
@@ -0,0 +1,39 @@
+package bankaccountapp;
+
+public class Savings extends Account {
+    // List properties specific to the Savings account
+    private double safetyDepositBoxID;
+    private int safetyDepositBoxKey;
+
+
+    // Constructor to initialize settings for the Savings properties
+    public Savings(String name, String sSN, double initDeposit) {
+        super(name, sSN, initDeposit);
+        accountNumber = "1" + accountNumber;
+        setSafetyDepositBox();
+
+    }
+
+    @Override
+    public void setRate() {
+        rate = getBaseRate() - .25;
+    }
+
+
+    private void setSafetyDepositBox() {
+        safetyDepositBoxID = (int) (Math.random() * Math.pow(10,3));
+        safetyDepositBoxKey = (int) (Math.random() * Math.pow(10,3));
+    }
+
+
+    // List any methods specific to savings account
+    public void showInfo() {
+        System.out.println("ACCOUNT TYPE: Savings");
+        super.showInfo();
+        System.out.println("Your Savings Account Features" +
+                "\n Safety Deposit Box ID: " + safetyDepositBoxID +
+                "\n Saftey Deposit Box Key: " + safetyDepositBoxKey);
+
+
+    }
+}
